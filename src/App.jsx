b7cbc24d620ea439ff1224815748e8b88fac2993
mnosumon <mdnuruddin378350@gmail.com>
2025-10-14 14:12:52 +0700

    environment setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <h1>hello</h1>
+      <h1 className="bg-gray-300 font-roboto">hello bangladesh</h1>
+      <h1 className="bg-gray-300 font-inter">hello bangladesh</h1>
+      <h1 className="bg-gray-300 font-montserrat">hello bangladesh</h1>
+    </div>
+  );
+};
+
+export default App;
